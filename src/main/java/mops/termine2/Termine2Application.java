e4542041acb9c6f10f3edbfe0d39d0095c2192a6
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-03-03 14:14:49 +0100

    spring project initialized

---
+++ src/main/java/mops/termine2/Termine2Application.java
@@ -0,0 +1,13 @@
+package mops.termine2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Termine2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Termine2Application.class, args);
+    }
+
+}
