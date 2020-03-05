0965726a751ae5f1700ad779bb067eed0d8cba93
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-03-05 13:51:00 +0100

    Added getMapping,test and empty html file for umfragen-abtimmung.html

--- src/main/java/mops/termine2/Termine2Controller.java
+++ src/main/java/mops/termine2/Termine2Controller.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Termine2Controller {
-	@GetMapping("/")
-	String index(Model m) {
-		return "index";
-	}
+    @GetMapping("/")
+    String index(Model m) {
+        return "index";
+    }
+
+    @GetMapping("/umfragen-abstimmung")
+    String umfragenAbstimmung(Model m) {
+        return "umfragen-abstimmung";
+    }
 
 }
