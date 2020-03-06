5a34a6f8631f36e188a7e9292c5056cb741a015c
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-03-06 14:38:24 +0100

    Changed mockMvc to transient

--- src/test/java/mops/termine2/ControllerTest.java
+++ src/test/java/mops/termine2/ControllerTest.java
@@ -1,18 +1,18 @@
 package mops.termine2;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest
 public class ControllerTest {
 	
 	@Autowired
-	MockMvc mvc;
+	transient MockMvc mvc;
 	
 	@Test
 	void testIndex() throws Exception {
