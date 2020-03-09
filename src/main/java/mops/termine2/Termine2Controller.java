392bc0aef11c11c7066621b9b71dd2f6ae8e8a74
reallyuselesscode <hantaisha@gmail.com>
2020-03-09 15:34:15 +0100

    Added thymeleaf for dates and @SessionScope to controller

--- src/main/java/mops/termine2/Termine2Controller.java
+++ src/main/java/mops/termine2/Termine2Controller.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.SessionScope;
 
+@SessionScope
 @Controller
 @RequestMapping("/termine2")
 public class Termine2Controller {
