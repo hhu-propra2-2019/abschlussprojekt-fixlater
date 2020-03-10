22eb5c7ee328eca23e274658d217a03fc6689a75
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-03-10 14:52:14 +0100

    Added method loadByLink to TerminfindungService, added a method to find TerminfindungDB from DB by Link
    Co-authored-by: @reallyuselesscode , @Marcel297

--- src/main/java/mops/termine2/database/TerminfindungRepository.java
+++ src/main/java/mops/termine2/database/TerminfindungRepository.java
@@ -1,10 +1,15 @@
 package mops.termine2.database;
 
 import mops.termine2.database.entities.TerminfindungDB;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TerminfindungRepository extends CrudRepository<TerminfindungDB, Long> {
 	
+	@Query
+	List<TerminfindungDB> findByLink(String link);
 }
--- src/main/java/mops/termine2/services/TerminfindungService.java
+++ src/main/java/mops/termine2/services/TerminfindungService.java
@@ -2,10 +2,12 @@ package mops.termine2.services;
 
 import mops.termine2.database.TerminfindungRepository;
 import mops.termine2.database.entities.TerminfindungDB;
+import mops.termine2.enums.Modus;
 import mops.termine2.models.Terminfindung;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -31,11 +33,38 @@ public class TerminfindungService {
 			terminfindungDB.setGruppe(terminfindung.getGruppe());
 			terminfindungDB.setTermin(termin);
 			
+			if (terminfindung.getGruppe() != null) {
+				terminfindungDB.setModus(Modus.GRUPPE);
+			} else {
+				terminfindungDB.setModus(Modus.LINK);
+			}
+			
 			terminfindungRepo.save(terminfindungDB);
 		}
 	}
 	
 	public Terminfindung loadByLink(String link) {
+		List<TerminfindungDB> termineDB = terminfindungRepo.findByLink(link);
+		if (!termineDB.isEmpty()) {
+			Terminfindung terminfindung = new Terminfindung();
+			TerminfindungDB ersterTermin = termineDB.get(0);
+			
+			terminfindung.setTitel(ersterTermin.getTitel());
+			terminfindung.setBeschreibung(ersterTermin.getBeschreibung());
+			terminfindung.setOrt(ersterTermin.getOrt());
+			terminfindung.setLoeschdatum(ersterTermin.getLoeschdatum());
+			terminfindung.setFrist(ersterTermin.getFrist());
+			terminfindung.setGruppe(ersterTermin.getGruppe());
+			terminfindung.setLink(ersterTermin.getLink());
+			terminfindung.setErsteller(ersterTermin.getErsteller());
+			
+			List<LocalDateTime> terminMoeglichkeiten = new ArrayList<>();
+			for (TerminfindungDB termin : termineDB) {
+				terminMoeglichkeiten.add(termin.getTermin());
+			}
+			terminfindung.setVorschlaege(terminMoeglichkeiten);
+			return terminfindung;
+		}
 		return null;
 	}
 	
