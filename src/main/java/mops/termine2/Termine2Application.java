c40d6d74fd39a38b3468dca0e68378f543651aba
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-03-10 16:33:24 +0100

    Corrected indentation in Termine2Application
    Co-authored-by: Marcel297 <marcel.hi@t-online.de>
    Co-authored-by: reallyuselesscode <hantaisha@gmail.com>

--- src/main/java/mops/termine2/Termine2Application.java
+++ src/main/java/mops/termine2/Termine2Application.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-Termine2Application {
+public class Termine2Application {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Termine2Application.class, args);
