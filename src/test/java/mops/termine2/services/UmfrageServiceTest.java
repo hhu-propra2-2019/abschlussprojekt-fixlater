b18c78164f9b23ab4dc69d87d66a9d993645805c
Leon <lenue100@hhu.de>
2020-03-11 16:24:43 +0100

    Added test for method loadByErsteller
    Co-authored-by: Marcel297 <marcel.hi@t-online.de>

--- src/test/java/mops/termine2/services/UmfrageServiceTest.java
+++ src/test/java/mops/termine2/services/UmfrageServiceTest.java
@@ -21,6 +21,22 @@ import static org.mockito.Mockito.when;
 
 public class UmfrageServiceTest {
 	
+	private static final String TITEL = "Toller Titel";
+
+	private static final long MAXANTWORT = 13L;
+
+	private static final LocalDateTime LOESCHDATUM = LocalDateTime.of(1, 3, 1, 1, 1, 1, 1);
+
+	private static final String LINK = "BruderJakob";
+
+	private static final String GRUPPER = "FIXLATER";
+
+	private static final LocalDateTime FRIST = LocalDateTime.of(1, 1, 1, 1, 1, 1, 1);
+
+	private static final String ERSTELLER = "Me";
+
+	private static final String BESCHREIBUNG = "Tolle Beschreibung";
+
 	private transient UmfrageService service;
 	
 	private transient UmfrageRepository repository;
@@ -43,10 +59,10 @@ public class UmfrageServiceTest {
 	public void loadUmfrageByLinkMitDreiVorschlaegen() {
 		int anzahl = 3;
 		List<UmfrageDB> umfrageDBs = erstelleUmfrageDBListeGruppe(anzahl);
-		when(repository.findByLink("BruderJakob")).thenReturn(umfrageDBs);
+		when(repository.findByLink(LINK)).thenReturn(umfrageDBs);
 		Umfrage erwartet = erstelleBeispielUmfrage(anzahl);
 		
-		Umfrage ergebnis = service.loadByLink("BruderJakob");
+		Umfrage ergebnis = service.loadByLink(LINK);
 		
 		assertThat(ergebnis).isEqualTo(erwartet);
 	}
@@ -54,20 +70,35 @@ public class UmfrageServiceTest {
 	@Test
 	public void loadUmfrageByLinkNichtExistent() {
 		List<UmfrageDB> umfrageDBs = new ArrayList<UmfrageDB>();
-		when(repository.findByLink("BruderJakob")).thenReturn(umfrageDBs);
+		when(repository.findByLink(LINK)).thenReturn(umfrageDBs);
 		
-		Umfrage ergebnis = service.loadByLink("BruderJakob");
+		Umfrage ergebnis = service.loadByLink(LINK);
 		
 		assertThat(ergebnis).isEqualTo(null);
 	}
 	
 	@Test
 	public void loadUmfrageByLinkNull() {
-		when(repository.findByLink("BruderJakob")).thenReturn(null);
+		when(repository.findByLink(LINK)).thenReturn(null);
 		
-		Umfrage ergebnis = service.loadByLink("BruderJakob");
+		Umfrage ergebnis = service.loadByLink(LINK);
 		
 		assertThat(ergebnis).isEqualTo(null);
+	}
+	
+	@Test
+	public void loadUmfragenByErstellerEineUmfrageDreiVorschlaege() {
+		int anzahl = 3;
+		List<UmfrageDB> umfrageDBs = erstelleUmfrageDBListeGruppe(anzahl);
+		List<String> links = new ArrayList<String>();
+		links.add(LINK);
+		when(repository.findLinkByErsteller(ERSTELLER)).thenReturn(links);
+		when(repository.findByLink(LINK)).thenReturn(umfrageDBs);
+		Umfrage erwartet = erstelleBeispielUmfrage(anzahl);
+		
+		Umfrage ergebnis = service.loadByErsteller(ERSTELLER).get(0);
+		
+		assertThat(ergebnis).isEqualTo(erwartet);
 	}
 	
 	private List<UmfrageDB> erstelleUmfrageDBListeGruppe(int anzahl) {
@@ -76,15 +107,15 @@ public class UmfrageServiceTest {
 		for (String s : vorschlaege) {
 			UmfrageDB umfrageDB = new UmfrageDB();
 			umfrageDB.setAuswahlmoeglichkeit(s);
-			umfrageDB.setBeschreibung("Tolle Beschreibung");
-			umfrageDB.setErsteller("Me");
-			umfrageDB.setFrist(LocalDateTime.of(1, 1, 1, 1, 1, 1, 1));
-			umfrageDB.setGruppe("FIXLATER");
-			umfrageDB.setLink("BruderJakob");
-			umfrageDB.setLoeschdatum(LocalDateTime.of(1, 3, 1, 1, 1, 1, 1));
+			umfrageDB.setBeschreibung(BESCHREIBUNG);
+			umfrageDB.setErsteller(ERSTELLER);
+			umfrageDB.setFrist(FRIST);
+			umfrageDB.setGruppe(GRUPPER);
+			umfrageDB.setLink(LINK);
+			umfrageDB.setLoeschdatum(LOESCHDATUM);
 			umfrageDB.setMaxAntwortAnzahl(13L);
 			umfrageDB.setModus(Modus.GRUPPE);
-			umfrageDB.setTitel("Toller Titel");
+			umfrageDB.setTitel(TITEL);
 			umfrageDBs.add(umfrageDB);
 		}
 		return umfrageDBs;
@@ -93,14 +124,14 @@ public class UmfrageServiceTest {
 	
 	private Umfrage erstelleBeispielUmfrage(int anzahl) {
 		Umfrage umfrage = new Umfrage();
-		umfrage.setBeschreibung("Tolle Beschreibung");
-		umfrage.setErsteller("Me");
-		umfrage.setFrist(LocalDateTime.of(1, 1, 1, 1, 1, 1, 1));
-		umfrage.setGruppe("FIXLATER");
-		umfrage.setLink("BruderJakob");
-		umfrage.setLoeschdatum(LocalDateTime.of(1, 3, 1, 1, 1, 1, 1));
-		umfrage.setMaxAntwortAnzahl(13L);
-		umfrage.setTitel("Toller Titel");
+		umfrage.setBeschreibung(BESCHREIBUNG);
+		umfrage.setErsteller(ERSTELLER);
+		umfrage.setFrist(FRIST);
+		umfrage.setGruppe(GRUPPER);
+		umfrage.setLink(LINK);
+		umfrage.setLoeschdatum(LOESCHDATUM);
+		umfrage.setMaxAntwortAnzahl(MAXANTWORT);
+		umfrage.setTitel(TITEL);
 		List<String> vorschlaege = erstelleVorschlaege(anzahl);
 		umfrage.setVorschlaege(vorschlaege);
 		return umfrage;
