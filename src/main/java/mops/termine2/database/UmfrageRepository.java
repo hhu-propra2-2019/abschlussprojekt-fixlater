a94a7041ae903d716a80a87e8d904c2745e69783
Kathismarty <49496978+Kathismarty@users.noreply.github.com>
2020-03-12 16:29:35 +0100

    Added a method to generate a link and a method to check if a link is unique
    Co-Authored-By: juliaprw <juliaprw@users.noreply.github.com>

--- src/main/java/mops/termine2/database/UmfrageRepository.java
+++ src/main/java/mops/termine2/database/UmfrageRepository.java
@@ -1,10 +1,17 @@
 package mops.termine2.database;
 
+
 import mops.termine2.database.entities.UmfrageDB;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UmfrageRepository extends CrudRepository<UmfrageDB, Long> {
+	
+	@Query
+	List<UmfrageDB> findByLink(String link);
 	
 }
