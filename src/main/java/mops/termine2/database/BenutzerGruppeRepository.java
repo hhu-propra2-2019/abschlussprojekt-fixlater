c3a4e03b2831c0cfcbc85ad1a669d7cc19fae700
Thomas <32958425+Aurifex01@users.noreply.github.com>
2020-03-12 14:17:31 +0100

    Added method to get all groups of a user

--- src/main/java/mops/termine2/database/BenutzerGruppeRepository.java
+++ src/main/java/mops/termine2/database/BenutzerGruppeRepository.java
@@ -4,7 +4,10 @@ import mops.termine2.database.entities.BenutzerGruppeDB;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BenutzerGruppeRepository extends CrudRepository<BenutzerGruppeDB, Long> {
 	
+	List<BenutzerGruppeDB> findByBenutzer(String benutzer);
 }
