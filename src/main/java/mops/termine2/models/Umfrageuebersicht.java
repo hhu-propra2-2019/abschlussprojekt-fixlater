c714efdc760f78de9edc832659b11b3bee461d92
Thomas <32958425+Aurifex01@users.noreply.github.com>
2020-03-12 14:16:26 +0100

    Renamed 'teilgenommen' to 'abgeschlossen'

--- src/main/java/mops/termine2/models/Umfrageuebersicht.java
+++ src/main/java/mops/termine2/models/Umfrageuebersicht.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 public class Umfrageuebersicht {
 	
-	private List<Umfrage> teilgenommen;
+	private List<Umfrage> abgeschlossen;
 	
 	private List<Umfrage> offen;
 	
