720b78ad2e184b39528f16a9b303d9e94eff97a6
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-03-13 15:32:59 +0100

    added method abstimmen to TerminAntwortService
    Co-authored-by: Marcel297 <marcel.hi@t-online.de>

--- src/main/java/mops/termine2/services/TerminAntwortService.java
+++ src/main/java/mops/termine2/services/TerminAntwortService.java
@@ -1,6 +1,13 @@
 package mops.termine2.services;
 
 import mops.termine2.database.TerminfindungAntwortRepository;
+import mops.termine2.database.entities.TerminfindungAntwortDB;
+import mops.termine2.database.entities.TerminfindungDB;
+import mops.termine2.enums.Modus;
+import mops.termine2.models.Terminfindung;
+import mops.termine2.models.TerminfindungAntwort;
+
+import java.time.LocalDateTime;
 
 public class TerminAntwortService {
 	
@@ -12,5 +19,34 @@ public class TerminAntwortService {
 								TerminfindungService terminFService) {
 		this.terminfindungService = terminFService;
 		antwortRepo = terminfindungAntwortRepository;
+	}
+	
+	public void abstimmen(TerminfindungAntwort antwort, Terminfindung terminVorschlag) {
+		
+		antwortRepo.deleteAllByTerminfindungLinkAAndBenutzer(terminVorschlag.getLink(), antwort.getKuerzel());
+		
+		for (LocalDateTime termin : antwort.getAntworten().keySet()) {
+			TerminfindungAntwortDB db = new TerminfindungAntwortDB();
+			TerminfindungDB terminfindungDB = new TerminfindungDB();
+			terminfindungDB.setTitel(terminVorschlag.getTitel());
+			terminfindungDB.setErsteller(terminVorschlag.getErsteller());
+			terminfindungDB.setBeschreibung(terminVorschlag.getBeschreibung());
+			terminfindungDB.setOrt(terminVorschlag.getOrt());
+			terminfindungDB.setLink(terminVorschlag.getLink());
+			terminfindungDB.setFrist(terminVorschlag.getFrist());
+			terminfindungDB.setLoeschdatum(terminVorschlag.getLoeschdatum());
+			if (terminVorschlag.getGruppe() == null) {
+				terminfindungDB.setModus(Modus.LINK);
+			} else {
+				terminfindungDB.setModus(Modus.GRUPPE);
+			}
+			terminfindungDB.setTermin(termin);
+			
+			db.setAntwort(antwort.getAntworten().get(termin));
+			db.setBenutzer(antwort.getKuerzel());
+			db.setPseudonym(antwort.getPseudonym());
+			db.setTerminfindung(terminfindungDB);
+			antwortRepo.save(db);
+		}
 	}
 }
