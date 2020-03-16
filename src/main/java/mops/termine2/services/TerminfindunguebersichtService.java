abea5474c5a05fe4b26665a00c9a1f144c922a13
Thomas <Aurifex01@gmx.de>
2020-03-16 13:14:51 +0100

    Added new method to get all date polls of a user

--- src/main/java/mops/termine2/services/TerminfindunguebersichtService.java
+++ src/main/java/mops/termine2/services/TerminfindunguebersichtService.java
@@ -1,34 +1,29 @@
 package mops.termine2.services;
 
+import mops.termine2.authentication.Account;
+import mops.termine2.models.Gruppe;
+import mops.termine2.models.Terminfindung;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import mops.termine2.authentication.Account;
-import mops.termine2.models.Gruppe;
-import mops.termine2.models.Terminfindung;
-
 @Service
 public class TerminfindunguebersichtService {
 	
-	@Autowired
 	private TerminfindungService terminfindungService;
 	
-	@Autowired
 	private GruppeService gruppeService;
 	
+	public TerminfindunguebersichtService(TerminfindungService terminfindungService, GruppeService gruppeService) {
+		this.terminfindungService = terminfindungService;
+		this.gruppeService = gruppeService;
+	}
+	
 	public List<Terminfindung> loadOffeneTerminfindungenFuerBenutzer(Account account) {
-		List<Terminfindung> termine = new ArrayList<>();
+		List<Terminfindung> termine = getTermineVonBenutzer(account);
 		List<Terminfindung> offeneTermine = new ArrayList<>();
-		
-		List<Gruppe> gruppen = gruppeService.loadByBenutzer(account);
-		
-		for (Gruppe g : gruppen) {
-			termine.addAll(terminfindungService.loadByGruppeOhneTermine(g.getName()));
-		}
 		
 		for (Terminfindung termin : termine) {
 			if (termin.getFrist().compareTo(LocalDateTime.now()) > 0) {
@@ -40,14 +35,8 @@ public class TerminfindunguebersichtService {
 	}
 	
 	public List<Terminfindung> loadAbgeschlosseneTerminfindungenFuerBenutzer(Account account) {
-		List<Terminfindung> termine = new ArrayList<>();
+		List<Terminfindung> termine = getTermineVonBenutzer(account);
 		List<Terminfindung> abgeschlosseneTermine = new ArrayList<>();
-		
-		List<Gruppe> gruppen = gruppeService.loadByBenutzer(account);
-		
-		for (Gruppe g : gruppen) {
-			termine.addAll(terminfindungService.loadByGruppeOhneTermine(g.getName()));
-		}
 		
 		for (Terminfindung termin : termine) {
 			if (termin.getFrist().compareTo(LocalDateTime.now()) <= 0) {
@@ -56,6 +45,18 @@ public class TerminfindunguebersichtService {
 		}
 		
 		return abgeschlosseneTermine;
+	}
+	
+	private List<Terminfindung> getTermineVonBenutzer(Account account) {
+		List<Terminfindung> termine = new ArrayList<>();
+		
+		List<Gruppe> gruppen = gruppeService.loadByBenutzer(account);
+		
+		for (Gruppe g : gruppen) {
+			termine.addAll(terminfindungService.loadByGruppeOhneTermine(g.getName()));
+		}
+		
+		return termine;
 	}
 	
 }
