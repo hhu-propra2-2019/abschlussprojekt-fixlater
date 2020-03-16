a96656c8d6e9beaa94ddfca309a855eb288301fd
Jan <javan108@hhu.de>
2020-03-16 13:12:16 +0100

    Added package controller and an empty controller for abstimmungTermin

---
+++ src/main/java/mops/termine2/controller/abstimmungTerminController.java
@@ -0,0 +1,12 @@
+package mops.termine2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Controller
+@SessionScope
+@RequestMapping("/termine2/termine_abstimmung")
+public class abstimmungTerminController {
+	
+}
