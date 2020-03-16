382757e334b21a0037c6508230ab00663ef84996
Thomas <Aurifex01@gmx.de>
2020-03-16 13:15:53 +0100

    Added test for 'GruppeService'

---
+++ src/test/java/mops/termine2/GruppeServiceTest.java
@@ -0,0 +1,59 @@
+package mops.termine2;
+
+import mops.termine2.authentication.Account;
+import mops.termine2.database.BenutzerGruppeRepository;
+import mops.termine2.database.entities.BenutzerGruppeDB;
+import mops.termine2.models.Gruppe;
+import mops.termine2.services.GruppeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GruppeServiceTest {
+	
+	private transient GruppeService gruppeService;
+	
+	private transient BenutzerGruppeRepository benutzerGruppeRepository;
+	
+	@BeforeEach
+	public void setUp() {
+		benutzerGruppeRepository = mock(BenutzerGruppeRepository.class);
+		gruppeService = new GruppeService(benutzerGruppeRepository);
+	}
+	
+	@Test
+	public void testLoadByBenutzer() {
+		Account account = new Account("studentin", "abc@def.de", null, null);
+		List<String> groupNames = new ArrayList<>(Arrays.asList("Best group eva", "FIXME", "Last One :("));
+		
+		List<BenutzerGruppeDB> gruppenDB = new ArrayList<>();
+		List<Gruppe> gruppen = new ArrayList<>();
+		for (String name : groupNames) {
+			BenutzerGruppeDB gruppeDB = new BenutzerGruppeDB();
+			gruppeDB.setGruppe(name);
+			gruppenDB.add(gruppeDB);
+			
+			Gruppe gruppe = new Gruppe();
+			gruppe.setName(name);
+			gruppen.add(gruppe);
+		}
+		
+		when(benutzerGruppeRepository.findByBenutzer(account.getName())).thenReturn(gruppenDB);
+		
+		List<Gruppe> ergebnis = gruppeService.loadByBenutzer(account);
+		
+		for (int i = 0; i < gruppen.size(); i++) {
+			assertThat(gruppen.get(i).getId()).isEqualTo(ergebnis.get(i).getId());
+			assertThat(gruppen.get(i).getName()).isEqualTo(ergebnis.get(i).getName());
+		}
+		
+	}
+	
+}
