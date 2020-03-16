d59fba1eb8195db089b594dcfecf67c4b9296dd5
Marcel <marcel.hi@t-online.de>
2020-03-16 14:45:23 +0100

    Added first method to UmfrageAntwortService and added delete query to UmfrageAntwortRepository

--- src/main/java/mops/termine2/database/UmfrageAntwortRepository.java
+++ src/main/java/mops/termine2/database/UmfrageAntwortRepository.java
@@ -1,10 +1,16 @@
 package mops.termine2.database;
 
-import mops.termine2.database.entities.UmfrageAntwortDB;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import mops.termine2.database.entities.UmfrageAntwortDB;
 
 @Repository
 public interface UmfrageAntwortRepository extends CrudRepository<UmfrageAntwortDB, Long> {
+	
+	@Query("delete from UmfrageAntwortDB db where db.umfrage.link like :link and db.benutzer like :benutzer")
+	void deleteAllByUmfrageLinkAndBenutzer(@Param("link") String link, @Param("benutzer") String benutzer);
 	
 }
--- src/main/java/mops/termine2/services/UmfrageAntwortService.java
+++ src/main/java/mops/termine2/services/UmfrageAntwortService.java
@@ -3,6 +3,11 @@ package mops.termine2.services;
 import org.springframework.stereotype.Service;
 
 import mops.termine2.database.UmfrageAntwortRepository;
+import mops.termine2.database.entities.UmfrageAntwortDB;
+import mops.termine2.database.entities.UmfrageDB;
+import mops.termine2.enums.Modus;
+import mops.termine2.models.Umfrage;
+import mops.termine2.models.UmfrageAntwort;
 
 @Service
 public class UmfrageAntwortService {
@@ -13,4 +18,34 @@ public class UmfrageAntwortService {
 		antwortRepo = umfrageAntwortRepository;
 	}
 	
+	public void abstimmen(UmfrageAntwort antwort, Umfrage umfrage) {
+		
+		antwortRepo.deleteAllByUmfrageLinkAndBenutzer(umfrage.getLink(), antwort.getBenutzer());
+		
+		for (String vorschlag : antwort.getAntworten().keySet()) {
+			UmfrageAntwortDB umfrageAntwortDB = new UmfrageAntwortDB();
+			UmfrageDB umfrageDB = new UmfrageDB();
+			umfrageDB.setAuswahlmoeglichkeit(vorschlag);
+			umfrageDB.setBeschreibung(umfrage.getBeschreibung());
+			umfrageDB.setErsteller(umfrage.getErsteller());
+			umfrageDB.setFrist(umfrage.getFrist());
+			umfrageDB.setGruppe(umfrage.getGruppe());
+			umfrageDB.setLink(umfrage.getLink());
+			umfrageDB.setLoeschdatum(umfrage.getLoeschdatum());
+			umfrageDB.setMaxAntwortAnzahl(umfrage.getMaxAntwortAnzahl());
+			umfrageDB.setTitel(umfrage.getTitel());
+			if (umfrage.getGruppe() == null) {
+				umfrageDB.setModus(Modus.LINK);
+			} else {
+				umfrageDB.setModus(Modus.GRUPPE);
+			}
+			
+			umfrageAntwortDB.setAntwort(antwort.getAntworten().get(vorschlag));
+			umfrageAntwortDB.setBenutzer(antwort.getBenutzer());
+			umfrageAntwortDB.setPseudonym(antwort.getPseudonym());
+			umfrageAntwortDB.setUmfrage(umfrageDB);
+			
+			antwortRepo.save(umfrageAntwortDB);
+		}
+	}
 }
