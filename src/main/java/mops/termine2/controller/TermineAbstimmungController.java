d8da9777596aa6463848fb5b3730673ccb586633
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-03-17 15:22:12 +0100

    Added getMapping vor termine-abstimmung, formatted termine-abstimmung.html

--- src/main/java/mops/termine2/controller/TermineAbstimmungController.java
+++ src/main/java/mops/termine2/controller/TermineAbstimmungController.java
@@ -4,7 +4,14 @@ package mops.termine2.controller;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import mops.termine2.Konstanten;
+import mops.termine2.authentication.Account;
+import mops.termine2.models.LinkWrapper;
+import mops.termine2.models.Terminfindung;
+import mops.termine2.models.TerminfindungAntwort;
 import mops.termine2.services.AuthenticationService;
+import mops.termine2.services.GruppeService;
+import mops.termine2.services.TerminAntwortService;
+import mops.termine2.services.TerminfindungService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +21,8 @@ import org.springframework.web.context.annotation.SessionScope;
 
 import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 
 @Controller
 @SessionScope
@@ -23,7 +32,18 @@ public class TermineAbstimmungController {
 	private final transient Counter authenticatedAccess;
 	
 	@Autowired
+	private TerminfindungService terminfindungService;
+	
+	@Autowired
+	private TerminAntwortService terminAntwortService;
+	
+	@Autowired
 	private AuthenticationService authenticationService;
+	
+	@Autowired
+	private GruppeService gruppeService;
+	
+	private HashMap<LinkWrapper, Terminfindung> letzteTerminfindung = new HashMap<LinkWrapper, Terminfindung>();
 	
 	public TermineAbstimmungController(MeterRegistry registry) {
 		authenticatedAccess = registry.counter("access.authenticated");
@@ -31,12 +51,42 @@ public class TermineAbstimmungController {
 	
 	@GetMapping("/termine-abstimmung")
 	@RolesAllowed({Konstanten.ROLE_ORGA, Konstanten.ROLE_STUDENTIN})
-	public String termineAbstimmung(Principal p, Model m) {
+	public String termineAbstimmung(Principal p, Model m, String link) {
+		Account account;
 		if (p != null) {
 			m.addAttribute(Konstanten.ACCOUNT, authenticationService.createAccountFromPrincipal(p));
+			account = authenticationService.createAccountFromPrincipal(p);
+		} else {
+			return null;
 		}
+		
+		Terminfindung terminfindung = terminfindungService.loadByLinkMitTerminen(link);
+		if (terminfindung == null) {
+			return "404";
+		}
+		
+		if (terminfindung.getGruppe() != null
+				&& !gruppeService.accountInGruppe(account, terminfindung.getGruppe())) {
+			return "403";
+		}
+		
+		LocalDateTime now = LocalDateTime.now();
+		if (terminfindung.getFrist().isBefore(now)) {
+			return "termine-abstimmung";
+		}
+		
+		TerminfindungAntwort antwort = terminAntwortService.loadByBenutzerAndLink(account.getName(), link);
+		
+		LinkWrapper setLink = new LinkWrapper(link);
+		letzteTerminfindung.put(setLink, terminfindung);
+		m.addAttribute("terminfindung", terminfindung);
+		m.addAttribute("antworten");
+		
+		
 		authenticatedAccess.increment();
 		
 		return "termine-abstimmung";
 	}
+	
+	
 }
