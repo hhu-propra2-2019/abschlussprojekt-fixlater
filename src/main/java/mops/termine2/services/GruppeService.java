4eb4121e26f80cb5285ae4a93ae2ad422cc7bb8e
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-03-18 20:29:17 +0100

    Fixed some checkstyle issues

--- src/main/java/mops/termine2/services/GruppeService.java
+++ src/main/java/mops/termine2/services/GruppeService.java
@@ -9,10 +9,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class GruppeService {
