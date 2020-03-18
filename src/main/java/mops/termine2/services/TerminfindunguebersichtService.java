5379cabc3db903da82e4399861ff61f2562a2c5c
Thomas <Aurifex01@gmx.de>
2020-03-18 15:36:31 +0100

    Added methods to get all date polls of a group
    Co-Authored-By: reallyuselesscode <reallyuselesscode@users.noreply.github.com>

--- src/main/java/mops/termine2/services/TerminfindunguebersichtService.java
+++ src/main/java/mops/termine2/services/TerminfindunguebersichtService.java
@@ -1,18 +1,18 @@
 package mops.termine2.services;
 
+import mops.termine2.authentication.Account;
+import mops.termine2.models.Gruppe;
+import mops.termine2.models.Terminfindung;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import mops.termine2.authentication.Account;
-import mops.termine2.models.Gruppe;
-import mops.termine2.models.Terminfindung;
-
 @Service
 public class TerminfindunguebersichtService {
+	
 	@Autowired
 	private TerminfindungService terminfindungService;
 	
@@ -22,6 +22,36 @@ public class TerminfindunguebersichtService {
 	public TerminfindunguebersichtService(TerminfindungService terminfindungService, GruppeService gruppeService) {
 		this.terminfindungService = terminfindungService;
 		this.gruppeService = gruppeService;
+	}
+	
+	public List<Terminfindung> loadOffeneTerminfindungenFuerGruppe(String gruppe) {
+		List<Terminfindung> termine = new ArrayList<>();
+		List<Terminfindung> offeneTermine = new ArrayList<>();
+		
+		termine.addAll(terminfindungService.loadByGruppeOhneTermine(gruppe));
+		
+		for (Terminfindung termin : termine) {
+			if (termin.getFrist().compareTo(LocalDateTime.now()) > 0) {
+				offeneTermine.add(termin);
+			}
+		}
+		
+		return offeneTermine;
+	}
+	
+	public List<Terminfindung> loadAbgeschlosseneTerminfindungenFuerGruppe(String gruppe) {
+		List<Terminfindung> termine = new ArrayList<>();
+		List<Terminfindung> abgeschlosseneTermine = new ArrayList<>();
+		
+		termine.addAll(terminfindungService.loadByGruppeOhneTermine(gruppe));
+		
+		for (Terminfindung termin : termine) {
+			if (termin.getFrist().compareTo(LocalDateTime.now()) <= 0) {
+				abgeschlosseneTermine.add(termin);
+			}
+		}
+		
+		return abgeschlosseneTermine;
 	}
 	
 	public List<Terminfindung> loadOffeneTerminfindungenFuerBenutzer(Account account) {
