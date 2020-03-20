861fc0a8336cbb3802d6537baf8ac38c15e595a5
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-03-20 14:07:36 +0100

    added a toString method to LocalDateTimeManager

--- src/main/java/mops/termine2/util/LocalDateTimeManager.java
+++ src/main/java/mops/termine2/util/LocalDateTimeManager.java
@@ -1,6 +1,8 @@
 package mops.termine2.util;
 
 import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocalDateTimeManager {
@@ -17,4 +19,42 @@ public class LocalDateTimeManager {
 			}
 		}
 	}
+	
+	public static String toString(LocalDateTime time) {
+		int dayOfWeek = time.getDayOfWeek().getValue();
+		int dayOfMonth = time.getDayOfMonth();
+		Month month = time.getMonth();
+		int year = time.getYear();
+		
+		String weekday;
+		if (dayOfWeek == 0) {
+			weekday = "So.";
+		} else if (dayOfWeek == 1) {
+			weekday = "Mo.";
+		} else if (dayOfWeek == 2) {
+			weekday = "Di.";
+		} else if (dayOfWeek == 3) {
+			weekday = "Mi.";
+		} else if (dayOfWeek == 4) {
+			weekday = "Do.";
+		} else if (dayOfWeek == 5) {
+			weekday = "Fr.";
+		} else {
+			weekday = "Sa.";
+		}
+		
+		String datum = weekday + " " + dayOfMonth + "." + month.getValue() + "." + year;
+		String uhrzeit = time.getHour() + ":" + time.getMinute();
+		return datum + " " + uhrzeit;
+	}
+	
+	public static List<String> toString(List<LocalDateTime> times) {
+		List<String> result = new ArrayList<>();
+		for (LocalDateTime time : times) {
+			result.add(toString(time));
+		}
+		return result;
+	}
+	
 }
+
