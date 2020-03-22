9f0851260c8379aa51fad546e8ad762733b10297
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-03-22 12:42:53 +0100

    Changed sortTermine so it will use the sort method from Collections

--- src/main/java/mops/termine2/util/LocalDateTimeManager.java
+++ src/main/java/mops/termine2/util/LocalDateTimeManager.java
@@ -2,21 +2,14 @@ package mops.termine2.util;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LocalDateTimeManager {
 	
+	
 	public static void sortTermine(List<LocalDateTime> termine) {
-		
-		for (int i = 0; i < termine.size(); i++) {
-			for (int j = i; j < termine.size(); j++) {
-				if (termine.get(i).isAfter(termine.get(j))) {
-					LocalDateTime tmpTermin = termine.get(j);
-					termine.set(j, termine.get(i));
-					termine.set(i, tmpTermin);
-				}
-			}
-		}
+		Collections.sort(termine);
 	}
 	
 	public static String toString(LocalDateTime time) {
