aaf37c7438b7e7f328984ce341478412ab93f7e4
reallyuselesscode <hantaisha@gmail.com>
2020-03-23 13:00:22 +0100

    Added necessary parameters

--- src/main/java/mops/termine2/services/UmfragenuebersichtService.java
+++ src/main/java/mops/termine2/services/UmfragenuebersichtService.java
@@ -1,14 +1,17 @@
 package mops.termine2.services;
 
-import mops.termine2.authentication.Account;
-import mops.termine2.models.Gruppe;
-import mops.termine2.models.Umfrage;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import mops.termine2.authentication.Account;
+import mops.termine2.models.Gruppe;
+import mops.termine2.models.Umfrage;
 
 @Service
 public class UmfragenuebersichtService {
@@ -19,42 +22,48 @@ public class UmfragenuebersichtService {
 	@Autowired
 	private GruppeService gruppeService;
 	
+	@Autowired
+	private UmfrageAntwortService umfrageAntwortService;
+	
+	public UmfragenuebersichtService(UmfrageService umfrageService,
+				GruppeService gruppeService, UmfrageAntwortService umfrageAntwortService) {
+		this.umfrageService = umfrageService;
+		this.gruppeService = gruppeService;
+		this.umfrageAntwortService = umfrageAntwortService;
+	}
+	
 	/**
 	 * Geht die Umfragen durch und filtert nach offenen die zu einer Gruppe gehören
+	 * @param account
 	 * @param gruppe
 	 * @return eine Liste von offenen Umfragen nach Gruppe
 	 */
-	public List<Umfrage> loadOffeneUmfragenFuerGruppe(String gruppe) {
+	public List<Umfrage> loadOffeneUmfragenFuerGruppe(Account account, String gruppe) {
 		List<Umfrage> umfragen = new ArrayList<>();
-		List<Umfrage> offeneUmfragen = new ArrayList<>();
-		
 		umfragen.addAll(umfrageService.loadByGruppeOhneUmfragen(gruppe));
+		List<Umfrage> offeneUmfragen = filterOpenSurveys(umfragen);
 		
-		for (Umfrage umfrage : umfragen) {
-			if (umfrage.getFrist().compareTo(LocalDateTime.now()) > 0) {
-				offeneUmfragen.add(umfrage);
-			}
-		}
+		offeneUmfragen = offeneUmfragen.stream()
+				.sorted(Comparator.comparing(Umfrage::getFrist))
+				.collect(Collectors.toList());
+		
+		offeneUmfragen = setTeilgenommen(offeneUmfragen, account);
 		
 		return offeneUmfragen;
 	}
 	
 	/**
 	 * Geht die Umfragen durch und filtert nach abgeschlossenen die zu einer Gruppe gehören
+	 * @param account
 	 * @param gruppe
 	 * @return eine Liste von abgeschlossenen Umfragen nach Gruppe
 	 */
-	public List<Umfrage> loadAbgeschlosseneUmfragenFuerGruppe(String gruppe) {
+	public List<Umfrage> loadAbgeschlosseneUmfragenFuerGruppe(Account account, String gruppe) {
 		List<Umfrage> umfragen = new ArrayList<>();
-		List<Umfrage> abgeschlosseneUmfragen = new ArrayList<>();
-		
 		umfragen.addAll(umfrageService.loadByGruppeOhneUmfragen(gruppe));
+		List<Umfrage> abgeschlosseneUmfragen = filterClosedSurveys(umfragen);
 		
-		for (Umfrage umfrage : umfragen) {
-			if (umfrage.getFrist().compareTo(LocalDateTime.now()) <= 0) {
-				abgeschlosseneUmfragen.add(umfrage);
-			}
-		}
+		abgeschlosseneUmfragen = sortiereAbgeschlosseneUmfragen(abgeschlosseneUmfragen);
 		
 		return abgeschlosseneUmfragen;
 	}
@@ -65,32 +74,30 @@ public class UmfragenuebersichtService {
 	 * @return eine Liste von offenen Umfragen nach Nutzer
 	 */
 	public List<Umfrage> loadOffeneUmfragenFuerBenutzer(Account account) {
+		List<Umfrage> umfragen = getAllUmfragenVonBenutzer(account);
+		List<Umfrage> offeneUmfragen = filterOpenSurveys(umfragen);
 		
-		List<Umfrage> umfragen = getUmfragenVonBenutzer(account);
-		List<Umfrage> offeneUmfragen = new ArrayList<>();
+		offeneUmfragen = offeneUmfragen.stream()
+				.sorted(Comparator.comparing(Umfrage::getFrist))
+				.collect(Collectors.toList());
 		
-		for (Umfrage umfrage : umfragen) {
-			if (umfrage.getFrist().compareTo(LocalDateTime.now()) > 0) {
-				offeneUmfragen.add(umfrage);
-			}
-		}
+		offeneUmfragen = setTeilgenommen(offeneUmfragen, account);
 		
 		return offeneUmfragen;
 	}
 	
+
 	/**
 	 * Geht die Umfragen durch und filtert nach abgeschlossenen die zu einem Nutzer gehören
 	 * @param account
 	 * @return eine Liste von abgeschlossenen Umfragen nach Nutzer
 	 */
 	public List<Umfrage> loadAbgeschlosseneUmfragenFuerBenutzer(Account account) {
-		List<Umfrage> umfragen = getUmfragenVonBenutzer(account);
-		List<Umfrage> abgeschlosseneUmfragen = new ArrayList<>();
-		for (Umfrage umfrage : umfragen) {
-			if (umfrage.getFrist().compareTo(LocalDateTime.now()) <= 0) {
-				abgeschlosseneUmfragen.add(umfrage);
-			}
-		}
+		List<Umfrage> umfragen = getAllUmfragenVonBenutzer(account);
+		List<Umfrage> abgeschlosseneUmfragen = filterClosedSurveys(umfragen);
+		
+		abgeschlosseneUmfragen = sortiereAbgeschlosseneUmfragen(abgeschlosseneUmfragen);
+		
 		return abgeschlosseneUmfragen;
 	}
 	
@@ -103,5 +110,79 @@ public class UmfragenuebersichtService {
 		return umfragen;
 	}
 	
+	private List<Umfrage> filterOpenSurveys(List<Umfrage> umfragen) {
+		List<Umfrage> offeneUmfragen = new ArrayList<>();
+		for (Umfrage umfrage : umfragen) {
+			if (umfrage.getFrist().compareTo(LocalDateTime.now()) > 0) {
+				offeneUmfragen.add(umfrage);
+			}
+		}
+		return offeneUmfragen;
+	}
+	
+	private List<Umfrage> filterClosedSurveys(List<Umfrage> umfragen) {
+		List<Umfrage> abgeschlosseneUmfragen = new ArrayList<>();
+		for (Umfrage umfrage : umfragen) {
+			if (umfrage.getFrist().compareTo(LocalDateTime.now()) <= 0) {
+				abgeschlosseneUmfragen.add(umfrage);
+			}
+		}
+		return abgeschlosseneUmfragen;
+	}
+	
+	private List<Umfrage> setTeilgenommen(List<Umfrage> umfragen, Account account) {
+		for (Umfrage umfrage : umfragen) {
+			umfrage.setTeilgenommen(umfrageAntwortService
+					.hatNutzerAbgestimmt(account.getName(), umfrage.getLink()));
+		}
+		return umfragen;
+	}
+	
+	private List<Umfrage> sortiereAbgeschlosseneUmfragen(List<Umfrage> umfragen) {
+		List<Umfrage> umfragenInVergangenheit = new ArrayList<>();
+		List<Umfrage> umfragenInZukunft = new ArrayList<>();
+		
+		for (Umfrage umfrage : umfragen) {
+			if (umfrage.getFrist().compareTo(LocalDateTime.now()) <= 0) {
+				umfragenInVergangenheit.add(umfrage);
+			} else {
+				umfragenInZukunft.add(umfrage);
+			}
+		}
+		
+		umfragenInVergangenheit = umfragenInVergangenheit.stream()
+				.sorted(Comparator.comparing(Umfrage::getFrist))
+				.collect(Collectors.toList());
+		
+		umfragenInZukunft = umfragenInZukunft.stream()
+				.sorted(Comparator.comparing(Umfrage::getFrist))
+				.collect(Collectors.toList());
+		
+		List<Umfrage> umfragenSortiert = new ArrayList<>();
+		umfragenSortiert.addAll(umfragenInVergangenheit);
+		umfragenSortiert.addAll(umfragenInZukunft);
+		
+		return umfragenSortiert;
+	}
+	
+	private List<Umfrage> getAllUmfragenVonBenutzer(Account account) {
+		List<Umfrage> umfragen = getUmfragenVonBenutzer(account);
+		umfragen.addAll(umfrageService.loadAllBenutzerHatAbgestimmtOhneUmfrage(account.getName()));
+		return null;
+	}
+	
+	private List<Umfrage> distinct(List<Umfrage> umfragen) {
+		List<Umfrage> distinct = new ArrayList<>();
+		List<String> links = new ArrayList<>();
+		
+		for (Umfrage umfrage : umfragen) {
+			if (!links.contains(umfrage.getLink())) {
+				links.add(umfrage.getLink());
+				distinct.add(umfrage);
+			}
+		}
+		
+		return distinct;
+	}
 }
 
