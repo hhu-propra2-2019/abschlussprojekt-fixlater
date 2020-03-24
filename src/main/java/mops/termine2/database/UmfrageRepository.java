3647879f2934001f76e1ce641728724b761777c8
Marcel <marcel.hi@t-online.de>
2020-03-24 15:54:11 +0100

    Added transactional annotation to all delete methods in repositories
    Co-authored-by: lenue100 <lenue100@hhu.de>

--- src/main/java/mops/termine2/database/UmfrageRepository.java
+++ src/main/java/mops/termine2/database/UmfrageRepository.java
@@ -1,13 +1,15 @@
 package mops.termine2.database;
 
-import mops.termine2.database.entities.UmfrageDB;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import mops.termine2.database.entities.UmfrageDB;
 
 @Repository
 public interface UmfrageRepository extends CrudRepository<UmfrageDB, Long> {
@@ -20,11 +22,14 @@ public interface UmfrageRepository extends CrudRepository<UmfrageDB, Long> {
 	@Query("select db from UmfrageDB db where db.gruppeId = :gruppeId order by db.frist")
 	List<UmfrageDB> findByGruppeId(@Param("gruppeId") Long gruppeId);
 	
+	@Transactional
 	@Query("delete from UmfrageDB db where db.link like : link")
 	void deleteByLink(@Param("link") String link);
 	
+	@Transactional
 	void deleteByGruppeId(Long gruppeId);
 	
+	@Transactional
 	@Query("delete from UmfrageDB where loeschdatum < :timeNow")
 	void deleteOutdated(@Param("timeNow") LocalDateTime timeNow);
 	
