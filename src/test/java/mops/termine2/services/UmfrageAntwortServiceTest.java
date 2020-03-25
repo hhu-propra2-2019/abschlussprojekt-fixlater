5d900682691ff1a69129e75266b6f71312204ddc
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-03-25 18:10:16 +0100

    Updated tests for UmfrageAntwortService

--- src/test/java/mops/termine2/services/UmfrageAntwortServiceTest.java
+++ src/test/java/mops/termine2/services/UmfrageAntwortServiceTest.java
@@ -1,13 +1,5 @@
 package mops.termine2.services;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
 import mops.termine2.database.UmfrageAntwortRepository;
 import mops.termine2.database.UmfrageRepository;
 import mops.termine2.database.entities.UmfrageAntwortDB;
@@ -15,6 +7,13 @@ import mops.termine2.database.entities.UmfrageDB;
 import mops.termine2.enums.Antwort;
 import mops.termine2.models.Umfrage;
 import mops.termine2.models.UmfrageAntwort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -24,57 +23,63 @@ import static org.mockito.Mockito.when;
 
 public class UmfrageAntwortServiceTest {
 	
-	private static final String LINK = "BruderJakob";
+	public static final String LINK = "IchBinKeinMannFuerEineNacht,IchBleibeHoechstensEinZweiStunden";
 	
-	private static final String BENUTZER1 = "Me";
+	public static final String BENUTZER1 = "Julia";
 	
-	private UmfrageAntwortRepository antwortRepo;
+	private transient UmfrageAntwortRepository antwortRepo;
 	
-	private UmfrageRepository umfrageRepo;
+	private transient UmfrageAntwortService antwortService;
 	
-	private UmfrageAntwortService antwortService;
+	private transient UmfrageRepository umfrageRepo;
 	
 	@BeforeEach
-	public void setUp() {
+	private void setUp() {
 		antwortRepo = mock(UmfrageAntwortRepository.class);
 		umfrageRepo = mock(UmfrageRepository.class);
-		
 		antwortService = new UmfrageAntwortService(antwortRepo, umfrageRepo);
 	}
 	
 	@Test
-	public void saveAntwortFuerUmfrageMit3Moeglichkeiten() {
-		int anzahl = 3;
-		Umfrage umfrage = getBeispielUmfrage();
-		UmfrageAntwort umfrageAntwort = new UmfrageAntwort();
-		umfrageAntwort.setAntworten(getBeispielAntwortenAlleJa(anzahl));
-		umfrageAntwort.setBenutzer(BENUTZER1);
+	public void saveAntwortFuerUmfrageMit4Moeglichkeiten() {
+		Umfrage umfrage = getBeispielUmfrage(4);
+		UmfrageAntwort toSave = new UmfrageAntwort();
 		
-		antwortService.abstimmen(umfrageAntwort, umfrage);
+		toSave.setAntworten(getBeispielAntwortenAlleJa(4));
+		toSave.setBenutzer(BENUTZER1);
+		when(umfrageRepo.findByLinkAndAuswahlmoeglichkeit(any(), any()))
+			.thenReturn(getBeispielUmfrageDBList(1).get(0));
+		antwortService.abstimmen(toSave, umfrage);
 		
-		Mockito.verify(antwortRepo, times(anzahl)).save(any());
-		Mockito.verify(antwortRepo, times(1)).deleteAllByUmfrageLinkAndBenutzer(any(), any());
+		Mockito.verify(antwortRepo, times(4)).save(any());
+		
 	}
 	
 	@Test
-	public void saveAntwortFuerUmfrageMit17Moeglichkeiten() {
-		int anzahl = 17;
-		Umfrage umfrage = getBeispielUmfrage();
-		UmfrageAntwort umfrageAntwort = new UmfrageAntwort();
-		umfrageAntwort.setAntworten(getBeispielAntwortenAlleJa(anzahl));
-		umfrageAntwort.setBenutzer(BENUTZER1);
+	public void saveAntwortFuerUmfrageMit49oeglichkeiten() {
+		Umfrage umfrage = getBeispielUmfrage(9);
+		UmfrageAntwort toSave = new UmfrageAntwort();
 		
-		antwortService.abstimmen(umfrageAntwort, umfrage);
+		toSave.setAntworten(getBeispielAntwortenAlleJa(9));
+		toSave.setBenutzer(BENUTZER1);
+		when(umfrageRepo.findByLinkAndAuswahlmoeglichkeit(any(), any()))
+			.thenReturn(getBeispielUmfrageDBList(1).get(0));
+		antwortService.abstimmen(toSave, umfrage);
 		
-		Mockito.verify(antwortRepo, times(anzahl)).save(any());
-		Mockito.verify(antwortRepo, times(1)).deleteAllByUmfrageLinkAndBenutzer(any(), any());
+		Mockito.verify(antwortRepo, times(9)).save(any());
+		
 	}
 	
-	//@Test
-	public void loadByBenutzerAndLinkEinBenutzer3Moeglichkeiten() {
-		int anzahl = 3;
+	@Test
+	public void loadByBenutzerUndLinkEinBenutzer4Moeglichkeiten() {
+		int anzahl = 4;
 		List<UmfrageAntwortDB> umfrageAntwortDBs = getBeispielAntwortDBList(anzahl, BENUTZER1);
-		when(antwortRepo.findByBenutzerAndUmfrageLink(BENUTZER1, LINK)).thenReturn(umfrageAntwortDBs);
+		List<UmfrageDB> umfrageDBs = getBeispielUmfrageDBList(anzahl);
+		
+		when(antwortRepo.findByBenutzerAndUmfrageLink(BENUTZER1, LINK))
+			.thenReturn(umfrageAntwortDBs);
+		when(umfrageRepo.findByLink(LINK))
+			.thenReturn(umfrageDBs);
 		UmfrageAntwort ergebnis = antwortService.loadByBenutzerAndLink(BENUTZER1, LINK);
 		UmfrageAntwort erwartet = getBeispielUmfrageAntwort(anzahl, BENUTZER1);
 		
@@ -82,85 +87,89 @@ public class UmfrageAntwortServiceTest {
 	}
 	
 	@Test
-	public void loadAllByLink3Moeglichkeiten3Benutzer() {
-		int anzahl = 3;
-		int anzahlBenutzer = 3;
-		List<UmfrageAntwortDB> umfrageAntwortDBs = getBeispieleAntwortDBList(anzahl, anzahlBenutzer);
-		when(antwortRepo.findAllByUmfrageLink(LINK)).thenReturn(umfrageAntwortDBs);
+	public void loadByLink4Benutzer() {
+		int anzahlBenutzer = 4;
+		List<UmfrageAntwortDB> beispielAntwort = getBeispieleAntwortDBList(anzahlBenutzer);
+		List<UmfrageDB> umfrageDBs = getBeispielUmfrageDBList(4);
+		when(antwortRepo.findAllByUmfrageLink(LINK)).thenReturn(beispielAntwort);
+		when(umfrageRepo.findByLink(LINK))
+			.thenReturn(umfrageDBs);
 		List<UmfrageAntwort> ergebnis = antwortService.loadAllByLink(LINK);
-		List<UmfrageAntwort> erwartet = getBeispieleUmfrageAntwort(anzahl, anzahlBenutzer);
+		List<UmfrageAntwort> erwartet = getBeispieleUmfrageAntwort(anzahlBenutzer);
 		
 		assertThat(ergebnis).isEqualTo(erwartet);
 	}
 	
-	@Test
-	public void loadAllByLink7Moeglichkeiten11Benutzer() {
-		int anzahl = 7;
-		int anzahlBenutzer = 11;
-		List<UmfrageAntwortDB> umfrageAntwortDBs = getBeispieleAntwortDBList(anzahl, anzahlBenutzer);
-		when(antwortRepo.findAllByUmfrageLink(LINK)).thenReturn(umfrageAntwortDBs);
-		List<UmfrageAntwort> ergebnis = antwortService.loadAllByLink(LINK);
-		List<UmfrageAntwort> erwartet = getBeispieleUmfrageAntwort(anzahl, anzahlBenutzer);
-		
-		assertThat(ergebnis).isEqualTo(erwartet);
-	}
-	
-	private Umfrage getBeispielUmfrage() {
-		Umfrage umfrage = new Umfrage();
-		umfrage.setLink(LINK);
-		return umfrage;
-	}
-	
-	private HashMap<String, Antwort> getBeispielAntwortenAlleJa(int anzahl) {
-		HashMap<String, Antwort> antworten = new HashMap<>();
-		for (int i = 0; i < anzahl; i++) {
-			antworten.put("Vorschlag " + i, Antwort.JA);
+	private List<UmfrageAntwort> getBeispieleUmfrageAntwort(int anzahlBenutzer) {
+		String name = "nameNr";
+		List<UmfrageAntwort> antworten = new ArrayList<>();
+		for (int i = 0; i < anzahlBenutzer; i++) {
+			antworten.add(getBeispielUmfrageAntwort(4, name + i));
 		}
 		return antworten;
 	}
 	
+	private List<UmfrageAntwortDB> getBeispieleAntwortDBList(int anzahlBenutzer) {
+		String name = "nameNr";
+		List<UmfrageAntwortDB> antwortenDb = new ArrayList<>();
+		for (int i = 0; i < anzahlBenutzer; i++) {
+			antwortenDb.addAll(getBeispielAntwortDBList(4, name + i));
+		}
+		return antwortenDb;
+	}
+	
 	private UmfrageAntwort getBeispielUmfrageAntwort(int anzahl, String benutzer) {
 		UmfrageAntwort umfrageAntwort = new UmfrageAntwort();
+		umfrageAntwort.setLink(LINK);
 		umfrageAntwort.setAntworten(getBeispielAntwortenAlleJa(anzahl));
 		umfrageAntwort.setBenutzer(benutzer);
-		umfrageAntwort.setLink(LINK);
-		umfrageAntwort.setTeilgenommen(true);
 		return umfrageAntwort;
 	}
 	
-	private List<UmfrageAntwort> getBeispieleUmfrageAntwort(int anzahl, int anzahlBenutzer) {
-		List<UmfrageAntwort> umfrageAntworten = new ArrayList<>();
-		String benutzername = "Benutzer";
-		for (int i = 0; i < anzahlBenutzer; i++) {
-			umfrageAntworten.add(getBeispielUmfrageAntwort(anzahl, benutzername + i));
+	private HashMap<String, Antwort> getBeispielAntwortenAlleJa(int anzahl) {
+		HashMap<String, Antwort> antworten = new HashMap<>();
+		for (int j = 0; j < anzahl; j++) {
+			String antwort = Integer.toString(j);
+			antworten.put(antwort, Antwort.JA);
 		}
-		return umfrageAntworten;
+		return antworten;
+	}
+	
+	private Umfrage getBeispielUmfrage(int anzahl) {
+		Umfrage umfrage = new Umfrage();
+		umfrage.setLink(LINK);
+		umfrage.setVorschlaege(new ArrayList<>());
+		for (int j = 0; j < anzahl; j++) {
+			umfrage.getVorschlaege().add(Integer.toString(j));
+		}
+		return umfrage;
 	}
 	
 	private List<UmfrageAntwortDB> getBeispielAntwortDBList(int anzahl, String benutzer) {
-		List<UmfrageAntwortDB> umfrageAntwortDBs = new ArrayList<>();
+		List<UmfrageAntwortDB> antwortDBs = new ArrayList<>();
 		HashMap<String, Antwort> antworten = getBeispielAntwortenAlleJa(anzahl);
 		for (String vorschlag : antworten.keySet()) {
-			UmfrageAntwortDB umfrageAntwortDB = new UmfrageAntwortDB();
+			UmfrageAntwortDB antwortDB = new UmfrageAntwortDB();
 			UmfrageDB umfrageDB = new UmfrageDB();
 			umfrageDB.setLink(LINK);
 			umfrageDB.setAuswahlmoeglichkeit(vorschlag);
-			umfrageAntwortDB.setAntwort(antworten.get(vorschlag));
-			umfrageAntwortDB.setBenutzer(benutzer);
-			umfrageAntwortDB.setUmfrage(umfrageDB);
-			
-			umfrageAntwortDBs.add(umfrageAntwortDB);
+			antwortDB.setUmfrage(umfrageDB);
+			antwortDB.setBenutzer(benutzer);
+			antwortDB.setAntwort(antworten.get(vorschlag));
+			antwortDBs.add(antwortDB);
 		}
-		return umfrageAntwortDBs;
+		
+		return antwortDBs;
 	}
 	
-	private List<UmfrageAntwortDB> getBeispieleAntwortDBList(int anzahl, int anzahlBenutzer) {
-		List<UmfrageAntwortDB> umfrageAntwortDBs = new ArrayList<>();
-		String benutzername = "Benutzer";
-		for (int i = 0; i < anzahlBenutzer; i++) {
-			umfrageAntwortDBs.addAll(getBeispielAntwortDBList(anzahl, benutzername + i));
+	private List<UmfrageDB> getBeispielUmfrageDBList(int anzahl) {
+		List<UmfrageDB> umfrageAntwortDBS = new ArrayList<>();
+		for (int i = 0; i < anzahl; i++) {
+			UmfrageDB umfrageDB = new UmfrageDB();
+			umfrageDB.setLink(LINK);
+			umfrageDB.setAuswahlmoeglichkeit(Integer.toString(i));
+			umfrageAntwortDBS.add(umfrageDB);
 		}
-		return umfrageAntwortDBs;
+		return umfrageAntwortDBS;
 	}
-	
 }
