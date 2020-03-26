b23f1cd17ed3ddda03f80d18348829a90dbf2ccb
Marcel <marcel.hi@t-online.de>
2020-03-26 23:38:52 +0100

    Added test for method deleteOutdated in UmfrageServiceTest

--- src/test/java/mops/termine2/services/UmfrageServiceTest.java
+++ src/test/java/mops/termine2/services/UmfrageServiceTest.java
@@ -182,6 +182,14 @@ public class UmfrageServiceTest {
 		assertThat(ergebnis).isEqualTo(erwartet);
 	}
 	
+	@Test
+	public void loescheAbgelaufene() {
+		service.deleteOutdated();
+		
+		Mockito.verify(umfrageAntwortRepository, times(1)).deleteByUmfrageLoeschdatumBefore(any());
+		Mockito.verify(umfrageRepository, times(1)).deleteByLoeschdatumBefore(any());
+	}
+	
 	private List<UmfrageDB> erstelleUmfrageDBListeGruppe(
 		int anzahl, int bIndex, int eIndex, int gIndex, int lIndex, int tIndex) {
 		List<UmfrageDB> umfrageDBs = new ArrayList<UmfrageDB>();
