5681d5193366cf786a997972077875aac2b15107
Thomas <Aurifex01@gmx.de>
2020-03-27 13:06:38 +0100

    Added large object annotations

--- src/main/java/mops/termine2/database/entities/TerminfindungDB.java
+++ src/main/java/mops/termine2/database/entities/TerminfindungDB.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import java.time.LocalDateTime;
 
 @Entity
@@ -33,6 +34,7 @@ public class TerminfindungDB {
 	
 	private Modus modus;
 	
+	@Lob
 	private String beschreibung;
 	
 	private Long gruppeId;
